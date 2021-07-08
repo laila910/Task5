6d6c04ddb4016007fe66768bf6a356a95f6ceb3d
Laila Ibrahim <laila457@h-eng.helwan.edu.eg>
2021-07-08 23:50:37 +0200

    Update index.php

--- index.php
+++ index.php
@@ -8,5 +8,10 @@ $SortByDesc = arsort($array);
 
 foreach( $array as $key => $Value){
     echo'The capital Of The Countrty'.'  '.$key.'is'.'  '.$Value .'<br>';
+
 }
+
+$str = '672319';
+//Split the string after 2 character and add a ":" after each split by  chunk_split() Function
+echo chunk_split($str,2,":");
 ?>
