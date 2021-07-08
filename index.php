ba1594a9c31c4c670fa02accec2fcdb89296e029
Laila Ibrahim <laila457@h-eng.helwan.edu.eg>
2021-07-09 00:10:40 +0200

    Update index.php

--- index.php
+++ index.php
@@ -14,12 +14,12 @@ foreach( $array as $key => $Value){
 $str = '672319';
 //The substr() function returns a part of a string.
 //substr(string,start,length)
+// $str1=substr($str,0,-1); //return the $str without the last character 
+// $str2=substr($str,-1,1);  //return the last character of $str
+//echo $str1.'<br>'; //return 67231
 
-$str2=substr($str,-1,1).'<br>';  //return the last character of $str
-$str1=substr($str,0,5);
-echo $str1.'<br>';
 //Split the string after 2 character and add a ":" after each split by  chunk_split() Function
-echo chunk_split($str,2,":");
+echo substr( chunk_split($str,2,":"),0,-1);
 //The substr() function returns a part of a string.
 //substr(string,start,length)
 ?>
